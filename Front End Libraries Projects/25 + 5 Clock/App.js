bffc89c20c12cf57986ea63f2b6375eca50adba8
Andres <andrespodadera87@gmail.com>
2022-03-05 23:04:31 +0100

    Clock finished

--- Front End Libraries Projects/25 + 5 Clock/App.js
+++ Front End Libraries Projects/25 + 5 Clock/App.js
@@ -1,17 +1,98 @@
 const App = () => {
   const [breakLength, setBreakLength] = React.useState(5);
   const [sessionLength, setSessionLength] = React.useState(25);
-  const {
-    handleBreakIncrease,
-    handleBreakDecrease,
-    handleSessionIncrease,
-    handleSessionDecrease,
-    play,
-    handlePlay,
-    handleReset,
-    title,
-    timerFormatter = () => "25:00",
-  } = {};
+  const [timeLeft, setTimeLeft] = React.useState(1500);
+  const [timingType, setTimingType] = React.useState("SESSION");
+
+  const [play, setPlay] = React.useState(false);
+
+  const timeout = setTimeout(() => {
+    if (timeLeft && play) {
+      setTimeLeft(timeLeft - 1);
+    }
+  }, 1000);
+
+  const handleBreakIncrease = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+  };
+
+  const handleBreakDecrease = () => {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1);
+    }
+  };
+
+  const handleSessionIncrease = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+      setTimeLeft(timeLeft + 60);
+    }
+  };
+
+  const handleSessionDecrease = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+      setTimeLeft(timeLeft - 60);
+    }
+  };
+
+  const handleReset = () => {
+    clearTimeout(timeout);
+    setPlay(false);
+    setTimeLeft(1500);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimingType("SESSION");
+    const audio = document.getElementById("beep");
+    audio.pause();
+    audio.currentTime = 0;
+  };
+
+  const handlePlay = () => {
+    clearTimeout(timeout);
+    setPlay(!play);
+  };
+
+  const resetTimer = () => {
+    const audio = document.getElementById("beep");
+    if (!timeLeft && timingType === "SESSION") {
+      setTimeLeft(breakLength * 60);
+      setTimingType("BREAK");
+      audio.play();
+    }
+    if (!timeLeft && timingType === "BREAK") {
+      setTimeLeft(sessionLength * 60);
+      setTimingType("SESSION");
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  };
+
+  const clock = () => {
+    if (play) {
+      timeout;
+      resetTimer();
+    } else {
+      clearTimeout(timeout);
+    }
+  };
+
+  React.useEffect(() => {
+    clock();
+  }, [play, timeLeft, timeout]);
+
+  const timeFormatter = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  const title = timingType === "SESSION" ? "Session" : "Break";
+
   return (
     <div>
       <div className="wrapper">
@@ -61,7 +142,7 @@ const App = () => {
         <div className="timer-wrapper">
           <div className="timer">
             <h2 id="timer-label">{title}</h2>
-            <h3 id="timer-left">{timerFormatter()}</h3>
+            <h3 id="time-left">{timeFormatter()}</h3>
           </div>
           <button onClick={handlePlay} id="start_stop">
             Start/Stop
@@ -80,4 +161,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("app"));
